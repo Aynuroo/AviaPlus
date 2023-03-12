6d39c5c6faebd15693ac68f466ceb77f42a240f9
Aynuroo <veliyevaaynur22@gmail.com>
2023-03-12 23:37:38 +0400

    Sql

---
+++ AviaPlus/AviaPlus/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AviaPlus.DAL
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option)
+        {
+
+        }
+    }
+}
